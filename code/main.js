1597a6b2bd7902e0d337bbe4edec54e2a6523564
Gitthuma <george.githuma@gmail.com>
2021-04-19 14:02:18 +0300

    Added README.md, index.html, style.css and main.js with loops-3 starter code

---
+++ code/main.js
@@ -0,0 +1,20 @@
+let i = 500;
+let para = document.createElement('p');
+
+function isPrime(num) {
+    for(let i = 2; i < num; i++) {
+        if(num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+// Add your code here
+
+
+// Don't edit the code below here!
+let section = document.querySelector('section');
+section.appendChild(para);
